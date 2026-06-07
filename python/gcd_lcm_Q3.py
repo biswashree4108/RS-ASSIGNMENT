9b8ce400c67787b657e9b199b345758c17e39315
biswashree4108 <biswashreebehura4108@gmail.com>
2026-06-07 13:54:26 +0530

    gcd_lcm_Q3.py

--- python/gcd_lcm_Q3.py
+++ python/gcd_lcm_Q3.py
@@ -1,14 +1,10 @@
-import math
+from math import gcd
 
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-small = min(a, b)
-for i in range(small, 0, -1):
-    if a % i == 0 and b % i == 0:
-        gcd = i
-        break;
 
-lcm = (a * b) // gcd
+g = gcd(a, b)
+l = (a * b) // g
 
-print("GCD:", gcd)
-print("LCM:", lcm)
+print("GCD =", g)
+print("LCM =", l)
